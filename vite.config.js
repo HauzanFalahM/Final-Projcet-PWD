c9bb055f21127b8b09f66e703cfb38fc31741f53
HauzanFalahM <hauzanfalah12@gmail.com>
2026-07-01 17:52:14 +0700

    Fix: update vite config base path and vercel filesystem

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '', // Tambahkan baris ini agar path aset bersifat relatif dan aman di Vercel
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
